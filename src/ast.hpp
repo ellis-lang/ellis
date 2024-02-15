0c6bd13975ed9b7da416a2e731e6ed632e04aa04
jonathanBalasingham <jonbalasin@gmail.com>
2024-02-15 21:48:39 +0000

    add llvm inegration

--- src/ast.hpp
+++ src/ast.hpp
@@ -10,6 +10,18 @@
 #include <string>
 #include <utility>
 #include <vector>
+#include "llvm-18/llvm/ADT/APFloat.h"
+#include "llvm-18/llvm/ADT/STLExtras.h"
+#include "llvm-18/llvm/IR/BasicBlock.h"
+#include "llvm-18/llvm/IR/Constants.h"
+#include "llvm-18/llvm/IR/DerivedTypes.h"
+#include "llvm-18/llvm/IR/Function.h"
+#include "llvm-18/llvm/IR/IRBuilder.h"
+#include "llvm-18/llvm/IR/LLVMContext.h"
+#include "llvm-18/llvm/IR/Module.h"
+#include "llvm-18/llvm/IR/Type.h"
+#include "llvm-18/llvm/IR/Verifier.h"
+
 
 
 class AST {
